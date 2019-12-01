4db0483cf28a94c3b14ff3fc44859bc1749a3cae
Krumplifozi <tamas.zsombok@stud.uni-corvinus.hu>
2019-12-01 22:48:50 +0100

    Setting of the button, labels, textboxes

--- VersionControl/Form1.cs
+++ VersionControl/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VersionControl.Entities;
 
 namespace VersionControl
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +21,21 @@ namespace VersionControl
             lblLastName.Text = Resource1.LastName; // label1
             lblFirstName.Text = Resource1.FirstName; // label2
             btnAdd.Text = Resource1.Add; // button1
+
+            // listbox1
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+            users.Add(u);
         }
     }
 }
